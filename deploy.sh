ab2b2280a798e4c467744435a94787391b67186d
kana03 <camille.naulet@hotmail.fr>
2022-10-21 15:44:55 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 set -e
 npm run build
 npm run db:serve
+cd dist
 
 cd dist
 
 git init
 git add -A
 git commit - 'new deployment'
-git push -f git@github.com:camillenaulet03.test_positionnement.git master:gh-pages
+git push -f git@github.com:camillenaulet03/test_positionnement.git master:gh-pages
 
 cd -
